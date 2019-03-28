0298a7df9d36fee044fe95f0e45f8e4b470ea96f
nop333 <nedissum@gmail.com>
2019-03-28 17:53:12 +0900

    .

--- src/app/layout/permission-settings/permission-settings.component.ts
+++ src/app/layout/permission-settings/permission-settings.component.ts
@@ -1,15 +1,89 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
+
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hyddddd', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'}, /*
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},*/
+];
+
+export interface WaitList {
+  name: string;
+  phone: string;
+}
+const WAIT_DATA: WaitList[] = [
+  { name: 'Person A John', phone: '0101111AAAA' },
+  { name: 'Person B Sarah', phone: '0102222BBBB' },
+];
+
+export interface ApprovedList {
+  name: string;
+  department: string;
+  position: string;
+  phone: string;
+  approvedBy: string;
+  approvedDate: string;
+
+}
+const APPROVED_DATA: ApprovedList[] = [
+  { name: 'Person C Elijah', department: 'Cdept', position: 'Third', phone: '0103333CCCC', approvedBy: 'Habced', approvedDate: 'Jan 1' },
+  { name: 'Person D Alyssa', department: 'Ddept', position: 'Fourth', phone: '0104444DDDD', approvedBy: 'Habced', approvedDate: 'March20' },
+];
+
+export interface EvictedList {
+  name: string;
+  department: string;
+  position: string;
+  phone: string;
+  evictedDate: string;
+  reason: string;
+
+}
+const EVICTED_DATA: EvictedList[] = [
+  { name: 'Person E Mike', department: 'Edept', position: 'Fifth', phone: '0105555EEEE', evictedDate: 'Jan 4', reason: 'Did Not Work' },
+  { name: 'Person F Grace', department:
+  'Fdept', position: 'Sixth', phone: '0106666FFFF', evictedDate: 'March 23', reason: 'Slept At Work' },
+];
+
 
 @Component({
   selector: 'app-permission-settings',
   templateUrl: './permission-settings.component.html',
   styleUrls: ['./permission-settings.component.css']
 })
-export class PermissionSettingsComponent implements OnInit {
+export class PermissionSettingsComponent  {
+
+  displayedColumns1: string[] = ['name', 'phone', ];
+  dataSource1 = new MatTableDataSource(WAIT_DATA);
+
+  displayedColumns2: string[] = ['name', 'department', 'position', 'phone', 'approvedBy', 'approvedDate' ];
+  dataSource2 = new MatTableDataSource(APPROVED_DATA);
+
+  displayedColumns3: string[] = ['name', 'department', 'position', 'phone', 'evictedDate', 'reason' ];
+  dataSource3 = new MatTableDataSource(EVICTED_DATA);
 
   constructor() { }
 
-  ngOnInit() {
+  applyFilter(filterValue: string) {
+    this.dataSource1.filter = filterValue.trim().toLowerCase();
+    this.dataSource2.filter = filterValue.trim().toLowerCase();
+    this.dataSource3.filter = filterValue.trim().toLowerCase();
   }
+
 
 }
